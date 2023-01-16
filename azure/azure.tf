8d5834acdfc014a165ea269237803b30acf41000
Mario Minardi <mario.minardi@hashicorp.com>
2023-01-16 10:19:40 -0700

    Update examples for projects general availability

--- azure/azure.tf
+++ azure/azure.tf
@@ -8,14 +8,6 @@ provider "azurerm" {
 provider "azuread" {
 }
 
-# Adds support for subjects with project information, and for subjects without project information.
-# All subject values in the future will include project information so this is a future proofing step
-# for users who are not yet using projects, and required for users who are.
-locals {
-  non_project_sub_prefix = "organization:${var.tfc_organization_name}:workspace:${var.tfc_workspace_name}"
-  project_sub_prefix = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}"
-}
-
 # Data source used to get the current subscription's ID.
 #
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/subscription
@@ -51,42 +43,16 @@ resource "azurerm_role_assignment" "tfc_role_assignment" {
 # workspace will be able to authenticate to Azure for the "plan" run phase.
 #
 # https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/application_federated_identity_credential
-resource "azuread_application_federated_identity_credential" "tfc_federated_credential_plan" {
-  application_object_id = azuread_application.tfc_application.object_id
-  display_name          = "my-tfc-federated-credential-plan"
-  audiences             = [var.tfc_azure_audience]
-  issuer                = "https://${var.tfc_hostname}"
-  subject               = "${local.non_project_sub_prefix}:run_phase:plan"
-}
-
-# Creates a federated identity credential which ensures that the given
-# workspace will be able to authenticate to Azure for the "apply" run phase.
-#
-# https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/application_federated_identity_credential
-resource "azuread_application_federated_identity_credential" "tfc_federated_credential_apply" {
-  application_object_id = azuread_application.tfc_application.object_id
-  display_name          = "my-tfc-federated-credential-apply"
-  audiences             = [var.tfc_azure_audience]
-  issuer                = "https://${var.tfc_hostname}"
-  subject               = "${local.non_project_sub_prefix}:run_phase:apply"
-}
-
-# Creates a federated identity credential which ensures that the given
-# workspace will be able to authenticate to Azure for the "plan" run phase
-# when project information is encoded in the subject.
-#
-# https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/application_federated_identity_credential
 resource "azuread_application_federated_identity_credential" "tfc_federated_credential_project_plan" {
   application_object_id = azuread_application.tfc_application.object_id
   display_name          = "my-tfc-federated-credential-project-plan"
   audiences             = [var.tfc_azure_audience]
   issuer                = "https://${var.tfc_hostname}"
-  subject               = "${local.project_sub_prefix}:run_phase:plan"
+  subject               = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}:run_phase:plan"
 }
 
 # Creates a federated identity credential which ensures that the given
-# workspace will be able to authenticate to Azure for the "apply" run phase
-# when project information is encoded in the subject.
+# workspace will be able to authenticate to Azure for the "apply" run phase.
 #
 # https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/application_federated_identity_credential
 resource "azuread_application_federated_identity_credential" "tfc_federated_credential_project_apply" {
@@ -94,6 +60,6 @@ resource "azuread_application_federated_identity_credential" "tfc_federated_cred
   display_name          = "my-tfc-federated-credential-project-apply"
   audiences             = [var.tfc_azure_audience]
   issuer                = "https://${var.tfc_hostname}"
-  subject               = "${local.project_sub_prefix}:run_phase:apply"
+  subject               = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}:run_phase:apply"
 }
 
