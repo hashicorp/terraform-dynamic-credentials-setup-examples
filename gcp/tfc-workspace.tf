2a67a94e0164a0a6e68da5b4af72c2de137ca0a3
gautam <gautambaghel93@gmail.com>
2023-01-24 19:39:46 -0800

    converted ws vars to var-sets

--- gcp/tfc-workspace.tf
+++ gcp/tfc-workspace.tf
@@ -4,32 +4,25 @@
 provider "tfe" {
   hostname = var.tfc_hostname
 }
-
-# Runs in this workspace will be automatically authenticated
-# to GCP with the permissions set in the GCP policy.
-#
-# https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/workspace
-resource "tfe_workspace" "my_workspace" {
-  name         = var.tfc_workspace_name
+resource "tfe_variable_set" "tfe_var_set" {
+  name         = "Google Cloud dynamic credentials"
+  description  = "GCP dynamic credentials applied to all workspaces."
+  global       = true
   organization = var.tfc_organization_name
 }
 
-# The following variables must be set to allow runs
-# to authenticate to GCP.
-#
-# https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/variable
 resource "tfe_variable" "enable_gcp_provider_auth" {
-  workspace_id = tfe_workspace.my_workspace.id
+  variable_set_id = tfe_variable_set.tfe_var_set.id
 
   key      = "TFC_GCP_PROVIDER_AUTH"
   value    = "true"
   category = "env"
-
+  
   description = "Enable the Workload Identity integration for GCP."
 }
 
 resource "tfe_variable" "tfc_gcp_project_number" {
-  workspace_id = tfe_workspace.my_workspace.id
+  variable_set_id = tfe_variable_set.tfe_var_set.id
 
   key      = "TFC_GCP_PROJECT_NUMBER"
   value    = data.google_project.project.number
@@ -39,7 +32,7 @@ resource "tfe_variable" "tfc_gcp_project_number" {
 }
 
 resource "tfe_variable" "tfc_gcp_workload_pool_id" {
-  workspace_id = tfe_workspace.my_workspace.id
+  variable_set_id = tfe_variable_set.tfe_var_set.id
 
   key      = "TFC_GCP_WORKLOAD_POOL_ID"
   value    = google_iam_workload_identity_pool.tfc_pool.workload_identity_pool_id
@@ -49,7 +42,7 @@ resource "tfe_variable" "tfc_gcp_workload_pool_id" {
 }
 
 resource "tfe_variable" "tfc_gcp_workload_provider_id" {
-  workspace_id = tfe_workspace.my_workspace.id
+  variable_set_id = tfe_variable_set.tfe_var_set.id
 
   key      = "TFC_GCP_WORKLOAD_PROVIDER_ID"
   value    = google_iam_workload_identity_pool_provider.tfc_provider.workload_identity_pool_provider_id
@@ -59,7 +52,7 @@ resource "tfe_variable" "tfc_gcp_workload_provider_id" {
 }
 
 resource "tfe_variable" "tfc_gcp_service_account_email" {
-  workspace_id = tfe_workspace.my_workspace.id
+  variable_set_id = tfe_variable_set.tfe_var_set.id
 
   key      = "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL"
   value    = google_service_account.tfc_service_account.email
@@ -67,6 +60,69 @@ resource "tfe_variable" "tfc_gcp_service_account_email" {
 
   description = "The GCP service account email runs will use to authenticate."
 }
+
+# # Runs in this workspace will be automatically authenticated
+# # to GCP with the permissions set in the GCP policy.
+# #
+# # https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/workspace
+# resource "tfe_workspace" "my_workspace" {
+#   name         = var.tfc_workspace_name
+#   organization = var.tfc_organization_name
+# }
+
+# # The following variables must be set to allow runs
+# # to authenticate to GCP.
+# #
+# # https://registry.terraform.io/providers/hashicorp/tfe/latfe_var_set/docs/resources/variable
+# resource "tfe_variable" "enable_gcp_provider_auth" {
+#   workspace_id = tfe_workspace.my_workspace.id
+
+#   key      = "TFC_GCP_PROVIDER_AUTH"
+#   value    = "true"
+#   category = "env"
+
+#   description = "Enable the Workload Identity integration for GCP."
+# }
+
+# resource "tfe_variable" "tfc_gcp_project_number" {
+#   workspace_id = tfe_workspace.my_workspace.id
+
+#   key      = "TFC_GCP_PROJECT_NUMBER"
+#   value    = data.google_project.project.number
+#   category = "env"
+
+#   description = "The numeric identifier of the GCP project"
+# }
+
+# resource "tfe_variable" "tfc_gcp_workload_pool_id" {
+#   workspace_id = tfe_workspace.my_workspace.id
+
+#   key      = "TFC_GCP_WORKLOAD_POOL_ID"
+#   value    = google_iam_workload_identity_pool.tfc_pool.workload_identity_pool_id
+#   category = "env"
+
+#   description = "The ID of the workload identity pool."
+# }
+
+# resource "tfe_variable" "tfc_gcp_workload_provider_id" {
+#   workspace_id = tfe_workspace.my_workspace.id
+
+#   key      = "TFC_GCP_WORKLOAD_PROVIDER_ID"
+#   value    = google_iam_workload_identity_pool_provider.tfc_provider.workload_identity_pool_provider_id
+#   category = "env"
+
+#   description = "The ID of the workload identity pool provider."
+# }
+
+# resource "tfe_variable" "tfc_gcp_service_account_email" {
+#   workspace_id = tfe_workspace.my_workspace.id
+
+#   key      = "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL"
+#   value    = google_service_account.tfc_service_account.email
+#   category = "env"
+
+#   description = "The GCP service account email runs will use to authenticate."
+# }
 
 # The following variables are optional; uncomment the ones you need!
 
